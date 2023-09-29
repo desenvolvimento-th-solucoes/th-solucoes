b5cc04afee4c6a21c9b8bdd1e1b8be8c35c838d4
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2023-09-29 10:42:00 -0300

    fi

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        port: process.env.PORT || 3320,
-        address: process.env.ADDRESS || "0.0.0.0"
-    }
+
 }
 
 module.exports = nextConfig
