a708fa714cab67018c1889f3e17c140819a28b4a
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2025-05-12 08:42:03 -0300

    hotfix: including the logging feature to debug.

--- pages/api/check.ts
+++ pages/api/check.ts
@@ -1,18 +1,18 @@
+import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const instance = axios.create({
+    baseURL: process.env.SERVICE_URL,
+    withCredentials: true
+})
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const request = await fetch(`${process.env.SERVICE_URL}/check`, {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        method: "GET",
-        credentials: "include",
-    })
-    if (!request.ok) {
+    const request = await instance.get(`${process.env.SERVICE_URL}/check`)
+    if (request.status !== 200) {
         console.log(request);
         return res.status(500).json({ error: "backend error" })
     }
-    const response = await request.json()
+    const response = await request.data;
     return res.status(request.status).json(response)
 }
 
