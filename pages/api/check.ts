5d3f76993f1ffd140699a14d7b5443d59f714fd4
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2025-05-12 09:11:15 -0300

    hotfix: including the logging feature to debug.

--- pages/api/check.ts
+++ pages/api/check.ts
@@ -1,18 +1,28 @@
-import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const instance = axios.create({
-    baseURL: process.env.SERVICE_URL,
-    withCredentials: true
-})
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const request = await instance.get(`/check`)
-        const response = await request.data;
-        return res.status(request.status).json(response)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({error: error})
+    const cookies = req.headers.cookie;
+
+    const headers: HeadersInit = {
+        "Content-Type": "application/json",
+        ...(cookies ? { "Cookie": cookies } : {})
+    };
+
+    const request = await fetch(`${process.env.SERVICE_URL}/check`, {
+        headers: headers,
+        method: "GET",
+        credentials: "include",
+    })
+    if (!request.ok) {
+        console.log(request);
+        return res.status(500).json({ error: "backend error" })
     }
+    const response = await request.json()
+    return res.status(request.status).json(response)
 }
+
+export const config = {
+    api: {
+        bodyParser: true,
+    },
+};
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,15 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { instance } from "./check";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const request = await instance.post(`/login`, {
+    const cookies = req.headers.cookie;
+
+    const headers: HeadersInit = {
+        "Content-Type": "application/json",
+        ...(cookies ? { "Cookie": cookies } : {})
+    };
+
+    const request = await fetch(`${process.env.SERVICE_URL}/login`, {
+        headers: headers,
+        method: "POST",
+        body: JSON.stringify({
             email: req.body.email,
             password: req.body.password
         })
-        const response = await request.data;
-        return res.status(request.status).json(response);
-    } catch (error) {
-        return res.status(502).json(error)
+    })
+    if (!request.ok) {
+        return res.status(500).json({ error: "backend error" })
     }
+    const response = await request.json()
+    return res.status(request.status).json(response)
 }
+
+export const config = {
+    api: {
+        bodyParser: true,
+    },
+};
