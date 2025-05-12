21adb94950eebd3f828e926f7459fb0c0564d08a
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2025-05-12 08:57:04 -0300

    hotfix: including the logging feature to debug.

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -4,10 +4,8 @@ import { instance } from "./check";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const request = await instance.post(`/login`, {
-            body: {
-                email: req.body.email,
-                password: req.body.password
-            }
+            email: req.body.email,
+            password: req.body.password
         })
         const response = await request.data;
         return res.status(request.status).json(response);
